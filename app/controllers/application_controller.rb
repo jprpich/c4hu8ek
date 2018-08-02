00f7c7bd69471ae17c8d9cc6b1289d559dfd6780
Joshua <joshuaprpich@gmail.com>
2018-08-02 16:02:26 -0500

    solve problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def basic_auth
-     authenticate_or_request_with_http_basic do |username, token|
-       user = User.find_by_email(username)
-       if user.api_token == token
-         sign_in user
-       end
-     end
-   end
+    user = User.find_by_email(request.headers['X-User-Email'])
+    if user.api_token == request.headers['X-Api-Token']
+      sign_in user
+    end
+  end
+
 end
